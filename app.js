419f9439a2846f85eef566dd2ad4ee737d3f5a70
imedboumalek <imedboumalek@gmail.com>
2020-09-11 16:18:42 +0100

    small layout fixes

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 });
+
+function SmoothVerticalScrolling(e, time, where) {
+    var eTop = e.getBoundingClientRect().top;
+    var eAmt = eTop / 100;
+    var curTime = 0;
+    while (curTime <= time) {
+        window.setTimeout(SVS_B, curTime, eAmt, where);
+        curTime += time / 100;
+    }
+}
+
+function SVS_B(eAmt, where) {
+    if (where == "center" || where == "")
+        window.scrollBy(0, eAmt / 2);
+    if (where == "top")
+        window.scrollBy(0, eAmt);
+}
