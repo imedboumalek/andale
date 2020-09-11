f1533202657cfc62f02ccf6d775947e6f50262bd
imedboumalek <imedboumalek@gmail.com>
2020-09-11 19:54:13 +0100

    testing the smooth scroll

--- app.js
+++ app.js
@@ -15,20 +15,6 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 });
-
-function SmoothVerticalScrolling(e, time, where) {
-    var eTop = e.getBoundingClientRect().top;
-    var eAmt = eTop / 100;
-    var curTime = 0;
-    while (curTime <= time) {
-        window.setTimeout(SVS_B, curTime, eAmt, where);
-        curTime += time / 100;
-    }
-}
-
-function SVS_B(eAmt, where) {
-    if (where == "center" || where == "")
-        window.scrollBy(0, eAmt / 2);
-    if (where == "top")
-        window.scrollBy(0, eAmt);
-}
+const scroll = new SmoothScroll('.menu a[href*="#"]', {
+    speed: 900
+});
