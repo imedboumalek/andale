cf4d0a86fd722b5f19bcf6ce855508d2a1073bcd
imedboumalek <imedboumalek@gmail.com>
2020-09-11 11:17:34 +0100

    fixed dropdown transition

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const menuBtn = document.querySelector('.menu-btn');
+const dropDown = document.querySelector('.mobile-nav');
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
+        dropDown.classList.add('open');
         menuOpen = true;
 
 
     } else {
         menuBtn.classList.remove('open');
+        dropDown.classList.remove('open');
         menuOpen = false;
     }
 });
